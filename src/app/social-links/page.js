9b486369e6865f51e29c90785c118d9c282c1ab4
Jordan Moureau <jordanfromnovember@gmail.com>
2026-02-26 06:39:23 -0800

    pre order

--- src/app/social-links/page.js
+++ src/app/social-links/page.js
@@ -11,7 +11,7 @@ export default function SocialLinks() {
       <img className="avatar" src="/profile.JPG" />
       <h2>@authorjmoureau</h2>
       <div className="social">
-        <a href="https://www.amazon.com/kindle-dbs/hz/subscribe/ku?ref=abpmku_24_4_us_psr_gs_r_mn_rbhab_rbw_rbw_aEcbqxOYNp_rbts&gad_campaignid=9816223125&gbraid=0AAAAADl6rKyW3HRXNr0E_UKUDyMD926JV&gclid=CjwKCAiAkvDMBhBMEiwAnUA9BS85EvqPCo9ldUrSWNsrw1tv0axi12NJ3-065XRjJnQ1sZVYN0fyzhoCilQQAvD_BwE">
+        <a href="https://www.amazon.com/dp/B0GQ9CKKGN?ref=sp_email">
           ✨ Pre-Order Aster of Warhaven on Kindle ✨
         </a>
       </div>
